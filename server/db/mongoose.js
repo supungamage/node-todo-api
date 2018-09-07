23dde9a1d26d830cb14d6fcc2620d4c1e3bebf36
Supun Gamage <supun@ebuilder.local>
2018-09-07 16:50:58 +0530

    mlab mongo configued for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,13 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+
+var dbURL = 'mongodb://root:123abc@ds149252.mlab.com:49252/todo-app';
+
+if(process.env.PORT === 3000) {
+  dbURL = 'mongodb://localhost:27017/TodoApp';
+}
+
+mongoose.connect(dbURL, { useNewUrlParser: true });
 
 module.exports = {mongoose};
