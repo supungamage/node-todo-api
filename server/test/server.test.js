b1ed6c9d94f26799347373ee7d6212a5e5113028
supun <supun.gamage@ebuilder.com>
2018-09-07 14:08:21 +0530

    fetching todos and testing

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 var testTodos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -68,4 +71,32 @@ describe('GET /todos', () => {
       })
       .end(done);
   });
+
+  describe('GET /todos/:id', () => {
+    it('Should return todo for valid id', (done) => {
+      request(app)
+        .get(`/todos/${testTodos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo.text).toBe(testTodos[0].text);
+        })
+        .end(done);
+    });
+
+    it('Should return 404 when no todo found', (done) => {
+      request(app)
+        .get(`/todos/${new ObjectID().toHexString()}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('Should return 404 for invalid todo id', (done) => {
+      request(app)
+        .get('/todos/123')
+        .expect(404)
+        .end(done);
+    });
+
+  });
+
 });
