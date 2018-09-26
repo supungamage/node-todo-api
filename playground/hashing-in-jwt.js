48ac77a768eb415c153951c1badd5db9f6ec779b
Supun Gamage <supun@ebuilder.local>
2018-09-26 12:32:51 +0530

    password hashing

--- playground/hashing-in-jwt.js
+++ playground/hashing-in-jwt.js
@@ -1,15 +1,34 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+////////////////////////////////
 
-var data = {
-  id: 10
-};
+var password = 'abc123';
 
-var token = jwt.sign(data, 'secretkey');
-console.log('token', token);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-var databack = jwt.verify(token, 'secretkey');
-console.log('databack', databack);
+var hashedPassword = '$2a$10$2t7I4diljx8U5mGvyXSO1eHA7VfvsJG.VpsEKQgq9wD5w6LYm881y'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+
+/////////////////////////////
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, 'secretkey');
+// console.log('token', token);
+//
+// var databack = jwt.verify(token, 'secretkey');
+// console.log('databack', databack);
 
 
 // var message = "test message";
