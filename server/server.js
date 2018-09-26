6faff53f4c8a190767e5ea4476188f5ed33659ac
Supun Gamage <supun@ebuilder.local>
2018-09-26 11:31:47 +0530

    authentication midleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 var mongoose = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./midleware/authenticate');
 
 var app = express();
 
@@ -98,6 +99,10 @@ app.post('/users', (req, res) => {
   }).then((token) => {
     res.header('x-auth', token).send(user);
   }).catch((err) => res.status(400).send(err));
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
